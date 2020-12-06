f593e6c620e747523d8dee3ccc116155292a5cdd
Alex <starcev.sash@gmail.com>
2020-12-06 22:38:09 +0300

    aa

--- program.py
+++ program.py
@@ -2,50 +2,55 @@ class Human:
     def __init__(self, name):
         self.name = name
 
-    # ответ по умолчанию для всех одинаковый, можно
-
-    # доверить его родительскому классу
-
     def answer_question(self, question):
-
         print('Очень интересный вопрос! Не знаю')
 
 
 class Student(Human):
-    #  метод ask_question() принимает параметр someone:
     def ask_question(Human, someone, question):
-        print(f'{someone}, {question}')
-    #  это объект, экземпляр класса Curator, Mentor или CodeReviewer,
-    #  которому Student задаёт вопрос;
-    #  параметр question — это просто строка
-    #  имя объекта и текст вопроса задаются при вызове метода ask_question
+        print(f'{someone.name}, {question}')
 
-        # запросите ответ на вопрос у someone
-        print()  # этот print выводит разделительную пустую строку
-        super().answer_question()
+        someone.answer_question(question)
+        print()
+
 
 
 
 
 class Curator(Human):
     def answer_question(self, question):
-        for i in question:
+        if question == 'мне грустненько, что делать?':
+            print('Держись, всё получится. Хочешь видео с котиками?')
 
-        # здесь нужно проверить, пришёл куратору знакомый вопрос или нет
-        # если да - ответить на него
-        # если нет - вызвать метод answer_question() у родительского класса
+        else:
+            super().answer_question(question)
 
 
-# объявите и реализуйте классы CodeReviewer и Mentor
-# следующий код менять не нужно, он работает, мы проверяли
+class CodeReviewer(Human):
+    def answer_question(self, question):
+        if question == 'что не так с моим проектом?':
+            print('О, вопрос про проект, это я люблю.')
+
+        else:
+            super().answer_question(question)
+
+
+class Mentor(Human):
+    def answer_question(self, question):
+        if question == 'мне грустненько, что делать?':
+            print('Отдохни и возвращайся с вопросами по теории.')
+
+        else:
+            print('Сейчас расскажу.')
+
 
 student1 = Student('Тимофей')
 curator = Curator('Марина')
 mentor = Mentor('Ира')
 reviewer = CodeReviewer('Евгений')
-
-
 friend = Human('Виталя')
+
+
 student1.ask_question(curator, 'мне грустненько, что делать?')
 student1.ask_question(mentor, 'мне грустненько, что делать?')
 student1.ask_question(reviewer, 'когда каникулы?')
